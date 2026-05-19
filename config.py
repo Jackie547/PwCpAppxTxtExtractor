c71bef1cdde0cdd9b59cdaa9633b71417e60e5a2
Jackie547 <holarick97@gmail.com>
2026-05-19 18:41:48 +0530

    Change API credentials in config.py
    Updated API credentials in config.py.

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0"
+api_id = "31605369"
+api_hash = "f8b54f0c81481ebf1593adebb073d844"
+bot_token = ""
 auth_users = [5840594311]
